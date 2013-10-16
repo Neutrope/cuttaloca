1848ad0e29848ddde1bd694db24a264f4b43d2ad
tbkn <tbkntb@gmail.com>
2013-10-16 15:29:12 +0900

    first commit （キャッシュ削除用スクリプト）

---
+++ delete.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf app/Plugin/TwigView/tmp/views/*
+rm -f app/tmp/cache/models/cu*
+rm -f app/tmp/cache/persistent/Admin/cu*
+rm -f app/tmp/cache/persistent/Front/cu*
+rm -f app/tmp/cache/persistent/Json/cu*
+rm -f app/tmp/cache/persistent/Stylist/cu*
+rm -f app/tmp/cache/persistent/User/cu*
